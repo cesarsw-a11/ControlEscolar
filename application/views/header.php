2c5341d184811c72fbd2020353088ace0715bdb9
menesescesar@hotmail.es <menesescesar@hotmail.es>
2021-12-10 20:18:39 -0600

    Corregir enrutamiento a home

--- application/views/header.php
+++ application/views/header.php
@@ -2,47 +2,55 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="<?= base_url("imagenes/Logo.jpg")?>" alt="..." height="36">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <?php if ($this->session->userdata("rol") == 3) { ?>
+            <a class="navbar-brand" href="<?= base_url("alumno") ?>">
+                <img src="<?= base_url("imagenes/Logo.jpg") ?>" alt="..." height="36">
+            </a>
+        <?php } elseif ($this->session->userdata("rol") == 2) { ?>
+            <a class="navbar-brand" href<?= base_url("docente") ?>">
+                <img src="<?= base_url("imagenes/Logo.jpg") ?>" alt="..." height="36">
+            </a>
+        <?php } else { ?>
+            <a class="navbar-brand" href="<?= base_url("administrador") ?>">
+                <img src="<?= base_url("imagenes/Logo.jpg") ?>" alt="..." height="36">
+            </a>
+        <?php } ?>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <?php if($this->session->userdata("rol") == 1 && $this->session->userdata("logged")){?>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Menú de registro
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="<?= base_url("administrador/alumnos")?>">Alumnos</a>
-                <a class="dropdown-item" href="<?= base_url("administrador/docentes")?>">Docentes</a>
-                <a class="dropdown-item" href="<?= base_url("administrador/materias")?>">Materias</a>
-                <a class="dropdown-item" href="<?= base_url("administrador/asignarMaterias")?>">Asignar materia al
-                    docente</a>
-                <a class="dropdown-item" href="<?= base_url("administrador/tomarInasistencias")?>">Inasistencias</a>
-            </div>
-        </div><?php } ?>
+        <?php if ($this->session->userdata("rol") == 1 && $this->session->userdata("logged")) { ?>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menú de registro
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="<?= base_url("administrador/alumnos") ?>">Alumnos</a>
+                    <a class="dropdown-item" href="<?= base_url("administrador/docentes") ?>">Docentes</a>
+                    <a class="dropdown-item" href="<?= base_url("administrador/materias") ?>">Materias</a>
+                    <a class="dropdown-item" href="<?= base_url("administrador/asignarMaterias") ?>">Asignar materia al
+                        docente</a>
+                    <a class="dropdown-item" href="<?= base_url("administrador/tomarInasistencias") ?>">Inasistencias</a>
+                </div>
+            </div><?php } ?>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
-                <?php if($this->session->userdata("rol") == 3){?>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/alumno">Home</a>
-                </li>
-                <?php }elseif($this->session->userdata("rol") == 2){ ?>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/docente">Home</a>
-                </li>
+                <?php if ($this->session->userdata("rol") == 3) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/alumno">Home</a>
+                    </li>
+                <?php } elseif ($this->session->userdata("rol") == 2) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/docente">Home</a>
+                    </li>
                 <?php } ?>
-                <?php if($this->session->userdata("rol") == 3){?>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/alumno/organigrama">Organigrama</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/alumno/kardex">Kardex</a>
-                </li>
-                <!-- <li class="nav-item">
+                <?php if ($this->session->userdata("rol") == 3) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/alumno/organigrama">Organigrama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/alumno/kardex">Kardex</a>
+                    </li>
+                    <!-- <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/alumno/altaMateria">+Alta Materia</a>
                 </li> -->
                 <?php } ?>
@@ -52,13 +60,12 @@
             </ul>
         </div>
         <ul class="nav navbar-nav navbar-right">
-            <li><a class="nav-link" href="<?= base_url('login/logout')?>"><span
-                        class="glyphicon glyphicon-ok"></span>Cerrar Sesión</a></li>
+            <li><a class="nav-link" href="<?= base_url('login/logout') ?>"><span class="glyphicon glyphicon-ok"></span>Cerrar Sesión</a></li>
         </ul>
     </div>
 </nav>
 <script>
-$(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-});
+    $(document).ready(function() {
+        $(".dropdown-toggle").dropdown();
+    });
 </script>
