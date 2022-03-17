066e95e3a1ed9d84ed5c5186009da59f65302ca9
menesescesar@hotmail.es <menesescesar@hotmail.es>
2022-03-17 11:42:30 -0600

    cambios2

--- application/views/administrador/asignarMaterias.php
+++ application/views/administrador/asignarMaterias.php
@@ -22,10 +22,12 @@
                                 <?php
                                 if ($docentes) {
                                     foreach ($docentes as $docente) { ?>
-                                        <option value="<?= $docente['iddocente'] ?>"><?= $docente['nombre'] . " " . $docente['appaterno'] . " " . $docente['apmaterno'] ?></option>
-                                    <?php }
+                                <option value="<?= $docente['iddocente'] ?>">
+                                    <?= $docente['nombre'] . " " . $docente['appaterno'] . " " . $docente['apmaterno'] ?>
+                                </option>
+                                <?php }
                                 } else { ?>
-                                    <option value="-1">Aun no hay docentes creados</option>
+                                <option value="-1">Aun no hay docentes creados</option>
                                 <?php } ?>
                             </select>
                         </div>
@@ -37,10 +39,11 @@
                                 <?php
                                 if ($materias) {
                                     foreach ($materias as $materia) { ?>
-                                        <option value="<?= $materia['idmateria'] ?>"><?= $materia['nombre'] . "-" . $materia['grupo'] ?></option>
-                                    <?php }
+                                <option value="<?= $materia['idmateria'] ?>">
+                                    <?= $materia['nombre'] . "-" . $materia['grupo'] ?></option>
+                                <?php }
                                 } else { ?>
-                                    <option value="-1">Aun no hay materias creadas</option>
+                                <option value="-1">Aun no hay materias creadas</option>
                                 <?php } ?>
                             </select>
                             <input type="text" class="form-control" id="formulario" name="formulario" hidden>
@@ -62,16 +65,19 @@
 
     <div class="d-flex flex-row-reverse">
         <div class="p-2" id="botonera">
-            <button <?php echo ($abrirCiclo == 1) ? 'style = "display:none";' : '' ?> id="btnAbrirCiclo" type="button" class="btn btn-warning">
+            <button <?php echo ($abrirCiclo == 1) ? 'style = "display:none";' : '' ?> id="btnAbrirCiclo" type="button"
+                class="btn btn-warning">
                 Abrir Altas Materias
             </button>
             <button id="btnSincronizar" type="button" class="btn btn-warning" onclick="sincronizarMateriasAlumnos()">
                 Sincronizar Materias
             </button>
-            <button <?php echo ($abrirCiclo == 1) ? 'style = "display:none";' : 'disabled ' ?> id="btnAsignar" type="button" class="btn btn-primary" onclick="ui_modalAsignarMateria()">
+            <button <?php echo ($abrirCiclo == 1) ? 'style = "display:none";' : 'disabled ' ?> id="btnAsignar"
+                type="button" class="btn btn-primary" onclick="ui_modalAsignarMateria()">
                 + Asignar materia a Docente
             </button>
-            <button <?php echo ($abrirCiclo == 1) ? '' : 'style = "display:none";' ?> id="btnAbrirNuevoCiclo" type="button" class="btn btn-warning">
+            <button <?php echo ($abrirCiclo == 1) ? '' : 'style = "display:none";' ?> id="btnAbrirNuevoCiclo"
+                type="button" class="btn btn-warning">
                 Abrir Nuevo Ciclo
             </button>
         </div>
@@ -93,45 +99,93 @@
 <?php $this->load->view("footer"); ?>
 <script src="<?= base_url('assets/scripts/adminMateriasDocentes.js') ?>"></script>
 <script>
-$(()=>{
-    $("#btnSincronizar").css("display","none")
+$(() => {
+    $("#btnSincronizar").css("display", "none")
 })
-    function sincronizarMateriasAlumnos() {
-        $.ajax({
-            url: base_url + 'administrador/cronActualizarMaterias',
-            type: 'POST',
-            success: function(data) {
+
+function sincronizarMateriasAlumnos() {
+    $.ajax({
+        url: base_url + 'administrador/cronActualizarMaterias',
+        type: 'POST',
+        success: function(data) {
 
 
-                swal(
-                    "Exito",
-                    "Se han sincronizado correctamente las materias.",
-                    "success"
-                );
+            swal(
+                "Exito",
+                "Se han sincronizado correctamente las materias.",
+                "success"
+            );
 
-            },
-            error: function(error, xhr, status) {
+        },
+        error: function(error, xhr, status) {
 
-                swal(
-                    "Error",
-                    "Error durante la sincronizacíon , intente nuevamente",
-                    "error"
-                );
-            }
-        });
-    }
-    $("#btnAbrirCiclo").click(() => {
-        $("#btnAsignar").prop("disabled", false)
-        $("#btnAbrirCiclo").css('display', 'none')
-        $("#botonera").append(`<button id="btnCerraCiclo" type="button" onclick="cerrarCiclo()" class="btn btn-warning">
+            swal(
+                "Error",
+                "Error durante la sincronizacíon , intente nuevamente",
+                "error"
+            );
+        }
+    });
+}
+$("#btnAbrirCiclo").click(() => {
+    $("#btnAsignar").prop("disabled", false)
+    $("#btnAbrirCiclo").css('display', 'none')
+    $("#botonera").append(`<button id="btnCerraCiclo" type="button" onclick="cerrarCiclo()" class="btn btn-warning">
                 Cerrar Altas Materias
             </button>`)
-    })
+})
 
-    function cerrarCiclo() {
+function cerrarCiclo() {
 
+    swal({
+            title: "Estas seguro de cerrar el ciclo, no se puede revertir?",
+            text: "Aun puedes eliminar esta acción.",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Si, estoy seguro",
+            cancelButtonText: "No, cancelar",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+        function(isConfirm) {
+            if (isConfirm) {
+                $.ajax({
+                    method: 'POST',
+                    data: {
+                        status: 1
+                    },
+                    url: base_url + "administrador/cerrarCiclo",
+                    success: function(response) {
+                        $("#btnAbrirCiclo").css('display', 'none')
+                        $("#btnAsignar").css('display', 'none')
+                        $("#btnCerraCiclo").css('display', 'none')
+                        $("#btnAbrirNuevoCiclo").css('display', 'inline')
+                        $("#btnSincronizar").css("display", "inline")
+                        swal(
+                            "Exito",
+                            "Ciclo cerrado correctamente.",
+                            "success"
+                        );
+
+                    }
+                })
+            } else {
+                swal("Cancelado", "Acción cancelada :)", "error");
+            }
+        });
+}
+
+$("#btnAbrirNuevoCiclo").click(() => {
+    var table = $('#tabla_materias').DataTable();
+    var longitud_tabla = table.data().count()
+
+    if (longitud_tabla > 0) {
+        swal("Advertencia", "Solo puede abrir nuevo ciclo ya que todas las materias sean " +
+            "capturadas :)", "warning");
+    } else {
         swal({
-                title: "Estas seguro de cerrar el ciclo, no se puede revertir?",
+                title: "Estas seguro de abrir un nuevo ciclo.",
                 text: "Aun puedes eliminar esta acción.",
                 type: "warning",
                 showCancelButton: true,
@@ -146,20 +200,12 @@ $(()=>{
                     $.ajax({
                         method: 'POST',
                         data: {
-                            status: 1
+                            status: 0
                         },
-                        url: base_url + "administrador/cerrarCiclo",
+                        url: base_url + "administrador/abrirCiclo",
                         success: function(response) {
-                            $("#btnAbrirCiclo").css('display', 'none')
-                            $("#btnAsignar").css('display', 'none')
-                            $("#btnCerraCiclo").css('display', 'none')
-                            $("#btnAbrirNuevoCiclo").css('display', 'inline')
-                            $("#btnSincronizar").css("display","inline")
-                            swal(
-                                "Exito",
-                                "Ciclo cerrado correctamente.",
-                                "success"
-                            );
+
+                            window.location.reload()
 
                         }
                     })
@@ -168,44 +214,5 @@ $(()=>{
                 }
             });
     }
-
-    $("#btnAbrirNuevoCiclo").click(() => {
-        var table = $('#tabla_materias').DataTable();
-        var longitud_tabla = table.data().count()
-
-        if (longitud_tabla > 0) {
-            swal("Advertencia", "Solo puede abrir nuevo ciclo ya que todas las materias sean " +
-                "capturadas :)", "warning");
-        } else {
-            swal({
-                    title: "Estas seguro de abrir un nuevo ciclo.",
-                    text: "Aun puedes eliminar esta acción.",
-                    type: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "Si, estoy seguro",
-                    cancelButtonText: "No, cancelar",
-                    closeOnConfirm: false,
-                    closeOnCancel: false
-                },
-                function(isConfirm) {
-                    if (isConfirm) {
-                        $.ajax({
-                            method: 'POST',
-                            data: {
-                                status: 0
-                            },
-                            url: base_url + "administrador/abrirCiclo",
-                            success: function(response) {
-
-                                window.location.reload()
-
-                            }
-                        })
-                    } else {
-                        swal("Cancelado", "Acción cancelada :)", "error");
-                    }
-                });
-        }
-    })
+})
 </script>
